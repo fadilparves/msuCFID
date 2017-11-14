993e2dc5c082fad89277b59f710de5d83d82c306
Mohamad Fadil <xymerz1@gmail.com>
2017-11-14 17:20:43 +0800

    DOne video

--- resources/views/videos/player.blade.php
+++ resources/views/videos/player.blade.php
@@ -1,3 +1,7 @@
  <video id="my-video" class="video-js" controls preload="auto" width="980" height="600">
-    <source src="http://localhost:8000/storage/app/public/35-api-tokens-with-devise-token-authenticatable.mp4" type='video/mp4'>
+    <source src="{{(url($path))}}" type='{{$mime}}'>
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a web browser that
+      <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+    </p>
   </video>
