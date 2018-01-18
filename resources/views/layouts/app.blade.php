7a8aa78dae88e80deb66b12a261be8b0d224048c
Mohamad Fadil <xymerz1@gmail.com>
2018-01-18 15:43:21 +0800

    Up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,18 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="https://v40.pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css"> </head>
-  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> 
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.5.6/tinymce.min.js"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/tinymce/4.5.6/jquery.tinymce.min.js"></script>
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
-  <link href="http://vjs.zencdn.net/6.2.8/video-js.css" rel="stylesheet">
-  <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
-  <script src="http://vjs.zencdn.net/6.2.8/video.js"></script>
+  <!-- <link rel="stylesheet" href="https://v40.pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css">  -->
+  <script type="text/javascript" src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css') }}">
+  <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
+  <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+  <script type="text/javascript" src="{{ asset('js/tinymce/js/tinymce/tinymce.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/tinymce/js/tinymce/jquery.tinymce.min.js') }}"></script>
+  <link href="{{ asset('css/video-js.css') }}" rel="stylesheet">
+  <script type="text/javascript" src="{{ asset('js/videojs-ie8.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/video.js') }}"></script>
   <link href="{{ asset('css/app.scss') }}" rel="stylesheet">
+</head>
 
 <body class="" background="https://preview.ibb.co/bNSQjm/photo_2017_12_20_03_17_32.jpg" style="background-repeat: no-repeat; background-size: 1500px 7000px;">
   <img class="text-center w-100" src="{{ URL::to('/') }}/msu1.jpg"/>
@@ -47,9 +47,9 @@
   <div id="app">
     @yield('content')
   </div>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
   <script src="{{ asset('js/app.js') }}"></script>
   <script type="text/javascript">
     tinymce.init({
